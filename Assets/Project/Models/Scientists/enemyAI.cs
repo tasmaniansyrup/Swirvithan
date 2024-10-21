6a86dedae31ad77b13666b0629dc8002fba9959b
Clyde Moustrap <carsoncreasy04@gmail.com>
2024-10-20 23:59:48 -0400

    Updated enemy combat
    Added health and enemy damage based on body part

--- Assets/Project/Models/Scientists/enemyAI.cs
+++ Assets/Project/Models/Scientists/enemyAI.cs
@@ -1,6 +1,7 @@
 using System;
 using System.Collections;
 using System.Collections.Generic;
+using Unity.VisualScripting;
 using UnityEngine;
 
 public class enemyAI : MonoBehaviour
@@ -14,6 +15,9 @@ public class enemyAI : MonoBehaviour
     public float enemySpeed;
     public Rigidbody enemyRB;
     public int enemyHealth;
+    public float knockback;
+    public bool gotHit;
+    public bool canBeHit;
 
 
     // Makes enemy alive when enemy first spawns
@@ -61,6 +65,42 @@ public class enemyAI : MonoBehaviour
                 gameObject.transform.LookAt(enemyLookAt);
                 enemyRB.velocity = Vector3.zero;
             }
+
+            checkHealth();
+
+            if(gotHit)
+            {
+                canBeHit = false;
+                IEnumerator rgh = resetGotHit(.1f);
+                StartCoroutine(rgh);
+            }
         }
+    }
+
+    public void checkHealth() 
+    {
+        if(enemyHealth <= 0)
+        {
+            enemyHealth = 0;
+
+            // Removes rotational restraints
+            enemyRB.constraints = RigidbodyConstraints.None;
+
+            // Creates a vector pointing from the player to the enemy for the knockback direction
+            Vector3 knockbackDir = player.transform.position - gameObject.transform.position;
+            knockbackDir.y = knockbackDir.y + 1;
+
+            // knocks enemy back
+            enemyRB.AddForce(knockbackDir.normalized * -knockback, ForceMode.Impulse);
+            Debug.Log("He dead!");
+        
+            isAlive = false;
+        }
+    }
+
+    public IEnumerator resetGotHit(float timeHit) 
+    {
+        yield return new WaitForSeconds(timeHit);
+        gotHit = false;
     }
 }
